28dea2ca9be9d4e612b42d8d6a2c47f2f46fa243
wxciaxiop-bit <wxciaxiop@gmail.com>
2026-02-27 15:27:50 +0630

    Update WINGOPRO.py

--- WINGOPRO.py
+++ WINGOPRO.py
@@ -27,7 +27,7 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-BOT_TOKEN = "8603187288:AAEnrXvN-WKutPZGBcnCnCfDw1RjMR0oQdg"
+BOT_TOKEN = "7952589380:AAFiZsU9Q8503AJ_xfpB_E1zUzh-EamWi18"
 BASE_URL = "https://api.bigwinqaz.com/api/webapi/"
 IGNORE_SSL = True
 WIN_LOSE_CHECK_INTERVAL = 2
